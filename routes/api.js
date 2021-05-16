6e558b0014549772db3ba77317ef79439339c479
unknown <Aghazjunaid>
2021-05-16 18:37:41 +0530

    api.js setup

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+apiRouter = express.Router();
+
+user = require('./user/user')();
+
+apiRouter.get('', (req, res) => {
+    res.status(200).send("Node api demo")
+})
+
+
+//============user api============================
+apiRouter.post('/register', user.registerUser);
+apiRouter.get('/login', user.loginUser);
